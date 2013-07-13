921728efa7b5581efbbbb173680485128d10da26
HariHaravelan <hariharavelan17@gmail.com>
2013-07-14 00:28:55 +0530

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover_OOP
+{
+    class Constants
+    {
+        public const char Left = 'L';
+        public const char Right = 'R';
+        public const char Move = 'M';
+        public const char North = 'N';
+        public const char South = 'S';
+        public const char East = 'E';
+        public const char West = 'W';
+    }
+}
